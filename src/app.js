82f96d13e8ff4e557abc9ee8d156eeb0f6d16a63
bartoszlokaj <bartoszlokaj@gmail.com>
2019-10-09 11:18:01 +0200

    mobile view of section introduction

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@
 
 // IMAGES
 import IntroductionBackground from "./img/introduction_background-1.png";
+import IntroductionBackground2 from "./img/introduction_background-2.png";
+import IntroductionBackgroundMobile from "./img/introduction_background-2.1.png";
 
 // FUNCTIONS
 import "../node_modules/jquery/dist/jquery.js";
